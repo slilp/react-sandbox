92cd6f3cb5cedc5587c18b468d56481619b2fe76
slil puangpoom <slil.puangpoom@gmail.com>
2022-04-04 00:16:56 +0700

    setup : enzymn for testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import Enzyme, { shallow } from "enzyme";
+import EnzymeAdapter from "@wojtekmaj/enzyme-adapter-react-17";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+test("renders without crashing", () => {
+  const wrapper = shallow(<App />);
+  // console.log(wrapper.debug());
+});
+
+test("renders without no component", () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.exists()).toBe(true);
 });
